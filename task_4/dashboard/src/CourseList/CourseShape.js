a2fd87ea1dd201245f6bce2787b1f2bbe1b0db1f
Birra Haile <birrahaile@gmail.com>
2023-06-29 16:34:10 +0300

    Duplicated

--- task_4/dashboard/src/CourseList/CourseShape.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-export const CourseShape = PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  credit: PropTypes.number.isRequired
-})
--- task_4/dashboard/src/Notifications/NotificationItemShape.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-export const NotificationItemShape = PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  html: PropTypes.shape({ __html: PropTypes.string}),
-  type: PropTypes.string.isRequired,
-  value: PropTypes.string
-})
